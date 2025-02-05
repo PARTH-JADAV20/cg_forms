aa63f9069921290a07963f3076331491da447203
Mohit Soni <mohitsoni2006ms@gmail.com>
2025-02-05 23:48:32 +0530

    initailizing userRoutes setup

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/mongodb.config.js";
+import userRoutes from "./routes/UserRoutes.js";
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ connectDB().catch(err => console.error(err));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use('/api/users', userRoutes);
 
 
 app.listen(PORT,()=>{
