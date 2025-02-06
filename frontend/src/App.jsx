fcd2b9454d293e543f2e4c6b8303f98f732c3422
Mohit Soni <mohitsoni2006ms@gmail.com>
2025-02-06 10:26:35 +0530

    initializing frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center text-4xl font-extrabold'>WELCOME TO CG FORMS</div>
+  )
+}
+
+export default App
